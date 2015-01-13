c91a2d32fe9b9bb649ca8bac9ae88941a9ffe591
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-01-13 18:19:43 +0300

    add PhotosContract

---
+++ app/src/main/java/ru/ifmo/md/extratask1/yfotki/provider/PhotosContract.java
@@ -0,0 +1,42 @@
+package ru.ifmo.md.extratask1.yfotki.provider;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Nikita Yaschenko on 13.01.15.
+ */
+public final class PhotosContract {
+
+    interface PhotoColumns {
+        String PHOTO_TITLE = "photo_title";
+        String PHOTO_CONTENT_URL = "photo_content_url";
+        String PHOTO_WATCH_URL = "photo_watch_url";
+    }
+
+    public static final String CONTENT_AUTHORITY = "ru.ifmo.md.extratask1.yfotki";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String PATH_PHOTOS = "photos";
+
+    public static class Photo implements PhotoColumns, BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_PHOTOS).build();
+
+        public static final String[] ALL_COLUMNS = {
+                BaseColumns._ID,
+                PhotoColumns.PHOTO_TITLE,
+                PhotoColumns.PHOTO_CONTENT_URL,
+                PhotoColumns.PHOTO_WATCH_URL,
+
+        };
+
+        public static Uri buildPhotoUri(String photoId) {
+            return CONTENT_URI.buildUpon().appendPath(photoId).build();
+        }
+
+    }
+
+}
