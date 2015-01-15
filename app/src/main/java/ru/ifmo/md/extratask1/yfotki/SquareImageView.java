cf082403f7e400928cb361b55efc78dac887c095
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-01-15 16:38:53 +0300

    add SquareImageView

---
+++ app/src/main/java/ru/ifmo/md/extratask1/yfotki/SquareImageView.java
@@ -0,0 +1,28 @@
+package ru.ifmo.md.extratask1.yfotki;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Nikita Yaschenko on 15.01.15.
+ */
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
